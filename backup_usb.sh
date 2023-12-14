52c8dd3abdaaf415c86efb2a915bc03fc91869d0
Ryan Sheehy <ryansheehy0@gmail.com>
2023-12-13 16:03:34 -0800

    Adds script to ask user to backup to another usb.

--- backup_usb.sh
+++ backup_usb.sh
@@ -16,6 +16,36 @@ cp -r "${usb_path}"/* ~/Backup/ &&
 
 echo "Backup completed successfully to ~/Backup"
 
-# Do you want to create a backup USB. Note: All the contents of this USB will be deleted. (y/n)
+# Do you want to create a backup USB. Note: All the contents of this USB will be trashed. (y/n):
   # Please plug in this backup USB. What is the file path to this backup USB:
-  # 
+  # gio trash contents of backup usb
+  # cp contents of ~/Backup into backup usb
+# Do you want to create another backup USB. Note: All the contents fo this USB will be trashed: (y/n):
+
+
+while [[ true ]]; do
+  first_backup_usb=1
+
+  if [[ $first_backup_usb == 0 ]]; then
+    prompt_suffix="nother"
+  else
+    prompt_suffix=""
+  fi
+
+  read -p "Do you want to create a${prompt_suffix} backup USB?
+    Note: All the contents of this USB will be trashed.
+    (y/n): " wants_backup_usb
+
+  case $wants_backup_usb in
+    [Yy])
+      first_backup_usb=0;;
+      # more code
+    [Nn])
+      break;;
+    *)
+      echo "Please use (y/n)."
+      continue;;
+  esac
+
+done
+
---
+++ test.sh
@@ -0,0 +1,40 @@
+first_backup_usb=1
+
+while [[ true ]]; do
+  if [[ $first_backup_usb == 0 ]]; then
+    prompt_suffix="nother"
+  else
+    prompt_suffix=""
+  fi
+
+  read -p "Do you want to create a${prompt_suffix} backup USB?
+    Note: All the contents of this USB will be trashed.
+    (y/n): " wants_backup_usb
+
+  case "$wants_backup_usb" in
+    [Yy])
+      first_backup_usb=0
+
+      read -ep "Please plug in this backup USB.
+        What is the file path to this backup USB: " backup_usb
+      if [ ! -d "$backup_usb" ]; then
+          echo "Error: The provided path is not a directory."
+          continue;;
+      fi
+
+      gio trash "$backup_usb"/* &&
+
+      cp -r ~/Backup/* "$backup_usb"/ &&
+
+      echo "Backup completed successfully to ${backup_usb}";;
+    [Nn])
+      break;;
+    *)
+      echo ""
+      echo "Please use (y/n)."
+      echo ""
+      continue;;
+  esac
+
+done
+
